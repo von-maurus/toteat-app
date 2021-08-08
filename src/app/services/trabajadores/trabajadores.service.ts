febb3e18bf89144def5f18a58aa0c0a0eefb3108
von-maurus <mx1807@gmail.com>
2021-08-08 07:22:57 -0400

    Conexion con el servidor realizada

--- src/app/services/trabajadores/trabajadores.service.ts
+++ src/app/services/trabajadores/trabajadores.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class VentasService {
+export class TrabajadoresService {
 
   URL_API = 'http://localhost:4000/api/ventas';
   constructor() { }
